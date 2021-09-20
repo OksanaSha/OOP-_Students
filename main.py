648a1519eb9bd466b8b2fa5901de5b81fef9c119
OksanaSha <oksanashatalova@bk.ru>
2021-09-20 19:49:30 +0300

    hw 3 for reviewers and lectures

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def rate_lecture(self, lecture, course, grade):
+        if isinstance(lecture, Lecture) and course in self.courses_in_progress and course in lecture.courses_attached:
+            lecture.grades[course] = lecture.grades.get(course, []) + [grade]
+        else:
+            print(f'Оценка не засчитана. Курс {course} недоступен для студента/лектора.')
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -19,7 +26,22 @@ class Mentor:
 
 
 class Lecture(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def find_average(self):
+        total_grades = 0
+        count_grades = 0
+        for grades in self.grades.values():
+            total_grades += sum(grades)
+            count_grades += len(grades)
+        return total_grades / count_grades
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\n' \
+              f'Средняя оценка за лекции: {self.find_average()}'
+        return res
 
 
 class Reviewer(Mentor):
@@ -32,16 +54,25 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
 
+cool_reviewer = Reviewer('Some', 'Buddy')
+
+# print(cool_reviewer)
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['C++']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_lecture = Lecture('Some', 'Buddy')
+cool_lecture.courses_attached += ['Python']
+cool_lecture.courses_attached += ['C++']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+best_student.rate_lecture(cool_lecture, 'Python', 10)
+best_student.rate_lecture(cool_lecture, 'Python', 10)
+best_student.rate_lecture(cool_lecture, 'Python', 10)
+best_student.rate_lecture(cool_lecture, 'C++', 8)
 
-print(best_student.grades)
+print(cool_lecture)
